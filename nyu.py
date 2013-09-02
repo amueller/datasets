2ba6dcf2e7ba473bc809221cb6f05c9c0714965a
Andy <amueller@ais.uni-bonn.de>
2013-09-02 18:54:42 +0200

    add colors, get_depth to nyu

--- nyu.py
+++ nyu.py
@@ -21,13 +21,22 @@ class NYUSegmentation(object):
         self.directory = directory
         self.prediction_path = directory
         self.classes = ["prop", "wall", "furniture", "ground" , "void"]
-        path_prefix = os.path.dirname(os.path.realpath(__file__))
-        colors = np.loadtxt(os.path.join(path_prefix, "pascal_colors.txt"))
+        #path_prefix = os.path.dirname(os.path.realpath(__file__))
+        #colors = np.loadtxt(os.path.join(path_prefix, "pascal_colors.txt"))
+        colors = np.array([[227, 172, 0], [185, 202, 202], [255, 94, 28],
+                           [9, 68, 83], [0, 0, 0]]) / 255.
         self.cmap = ListedColormap(colors)
-        self.void_label = 5
+        self.void_label = 4
 
-    def load_image(self, filename):
+    def get_image(self, filename):
         return imread(self.directory + "/input/%s_lab_image.png" % filename)
+
+    def get_depth(self, filename):
+        depth_file = self.directory + "/input/%s_depth_image.data" % filename
+        f = open(depth_file)
+        shape = np.fromfile(f, dtype=np.uint16, count=2)
+        depth_image = np.fromfile(f, dtype=np.float32).reshape(shape[::-1])
+        return depth_image
 
     def get_ground_truth(self, filename):
         image = imread(self.directory + "/prediction_all/%s_lab_image_groundTruth.png"
@@ -35,7 +44,7 @@ class NYUSegmentation(object):
         # hack around to get the label integers from the png
         gt_int = mapper[image[:, :, 0].ravel()]
         gt_int = gt_int.reshape(image.shape[:2])
-        return gt_int
+        return gt_int.astype(np.int)
 
     def get_split(self, which='train'):
         if which not in ["train", "val"]:
